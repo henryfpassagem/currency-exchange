4ce7de7631d8df2e11fec7cd0b01659b80b8d992
hpassagem <henryfpassagem>
2026-03-15 09:55:12 +0100

    add logic finished

---
+++ src/helpers.js
@@ -0,0 +1,35 @@
+function createLielement(currency){
+    const LiElement = document.createElement("li");
+    const dateElement = document.createElement("p");
+    const valueElement = document.createElement("span");
+
+    //timestamp is in seconds and must be in ms
+    //to show correct date (multiply by 1000)
+    const MILISECONDS = 1000;
+    const date = new Date(currency.timestamp * MILISECONDS);
+
+    dateElement.textContent = date.toLocaleDateString('en-US');
+    valueElement.textContent = currency.ask;
+
+    LiElement.appendChild(dateElement);
+    LiElement.appendChild(valueElement);
+
+    return LiElement
+};
+
+export function renderCurrenciesList(
+    currenciesList,
+    listElement,
+    titleElement
+) {
+    const { code, codein } = currenciesList[0];
+
+    titleElement.textContent = `Prices of ${code} in ${codein} 
+        from the last 30 days`;
+
+    listElement.innerContent = ``;
+
+    const liElements = currenciesList.map((currency) => createLielement(currency));
+
+    listElement.append(...liElements);
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,41 @@
-console.log('Hello World');
+// 1 - get the button and input html tags
+// 2 - Add the event of click in the button
+// 3 - Request the API
+// 4 - Render the data
+// 5 - Add error validation
+
+import Swal from "sweetalert2";
+import { renderCurrenciesList } from "./helpers";
+
+const buttonElement = document.querySelector(".btn-search");
+const inputElement = document.querySelector(".input-currency");
+const listElement = document.querySelector(".currencies-list");
+const titleElement = document.querySelector(".currencies-title");
+
+buttonElement.addEventListener('click', (event) => {
+    //Prevent submiting the form and reloading the page
+    event.preventDefault();
+    
+    const API_URL = "https://economia.awesomeapi.com.br/"
+    const API_ENDPOINT = `json/daily/${inputElement.value}/30`
+
+    fetch(`${API_URL}${API_ENDPOINT}`)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error("error fetching data");
+        })
+        .then((currenciesList) => {
+            renderCurrenciesList(
+                currenciesList,
+                listElement,
+                titleElement
+            );
+        })
+        .catch((error) => Swal.fire({
+            title: "Error",
+            text: "Can't get the prices",
+            icon: "error",
+        }));
+});
